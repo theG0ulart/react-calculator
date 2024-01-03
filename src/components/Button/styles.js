29dd84e039ee766b23301c1b15ce9456b09c4f2d
Lucas Goulart <lucas.goulart@wearedev.com.br>
2024-01-03 16:38:06 -0300

    :tada: Init

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #0000FF;
+    color: #FFFFFF;
+    font-size: 1.5rem;
+    font-weight: 700;
+    flex: 1;
+    height: 75px;
+
+
+    &:hover {
+        opacity: 0.5;
+    }
+    
+`
+
---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAE;
+
+    display: flex;
+    align-items: center;
+    
+
+
+    input {
+        margin-right: 10px;
+        color: black;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        border: 0px;
+        text-align: right;
+        
+        font-weight: bold;
+        font-size: 2.5rem;
+        font-family: 'Arial';
+    }
+
+
+`
